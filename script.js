9d4b5f49925ae6fa71b69f2d01a34f18aa51d4a6
Ziad Elmohager <ziadbobo78@gmail.com>
2025-08-10 22:50:55 +0300

    change

--- script.js
+++ script.js
@@ -59,3 +59,61 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 });
+
+
+let apiUrl = "https://ziaddemo.runasp.net/api/Contact/send";
+
+ const form = document.getElementById('contactForm');
+  const resultDiv = document.getElementById('result');
+  const resulterror = document.getElementById('result2');
+
+  form.addEventListener('submit', async function(e) {
+    e.preventDefault();
+
+    const name = form.name.value.trim();
+    const email = form.email.value.trim();
+    const message = form.message.value.trim();
+
+    if (name.length === 0 || name.length > 30) {
+resulterror.textContent = "Name is required and must not exceed 30 characters.";
+      return;
+    }
+    if (email.length === 0 || email.length > 50) {
+resulterror.textContent = "Email is required and must not exceed 50 characters.";
+      return;
+    }
+    if (message.length === 0 || message.length > 350) {
+resulterror.textContent = "Message is required and must not exceed 350 characters.";
+      return;
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      resulterror.textContent = "Please Enter Your Correct Email";
+      return;
+    }
+
+    const data = { name, email, message };
+
+    try {
+      const response = await fetch(apiUrl, {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+      if (response.ok) {
+        resultDiv.textContent = "Thanks for Send Me";
+        setTimeout(()=>{
+          resultDiv.textContent = "";
+        },5000)
+        form.reset();
+      } else {
+        const error = await response.json();
+ resulterror.textContent = "Server Error: " + (error.details || error.error || response.statusText);      }
+    } catch (err) {
+      resulterror.textContent = "Error in Server ";
+      console.error(err);
+    }
+  });
